9bcd0c5f55538e54c1bf7967a6aa617c8c1af75b
Rafael Figueiredo <mail.rafaelfigueiredo@gmail.com>
2021-04-03 10:50:53 -0300

    <feature> Identity configurado, separado em dois dbContext e alterando nomes das tabelas.

---
+++ ControleContas/ControleContas.Infra.Data/Migrations/UsersDb/20210403014741_AdicionarIdentity.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using System;
+
+namespace ControleContas.Infra.Data.Migrations.UsersDb
+{
+    public partial class AdicionarIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Claims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Claims", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Logins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ProviderKey = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logins", x => new { x.LoginProvider, x.ProviderKey });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Papeis",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Papeis", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PapelClaim",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoleId = table.Column<string>(type: "text", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PapelClaim", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsuarioPapel",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    RoleId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuarioPapel", x => new { x.UserId, x.RoleId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    LastChangeMoment = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    RegisterMoment = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "text", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsuarioToken",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    LoginProvider = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuarioToken", x => new { x.UserId, x.LoginProvider, x.Name });
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Claims_UserId",
+                table: "Claims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Logins_UserId",
+                table: "Logins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "Papeis",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PapelClaim_RoleId",
+                table: "PapelClaim",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioPapel_RoleId",
+                table: "UsuarioPapel",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "Usuarios",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "Usuarios",
+                column: "NormalizedUserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Claims");
+
+            migrationBuilder.DropTable(
+                name: "Logins");
+
+            migrationBuilder.DropTable(
+                name: "Papeis");
+
+            migrationBuilder.DropTable(
+                name: "PapelClaim");
+
+            migrationBuilder.DropTable(
+                name: "UsuarioPapel");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "UsuarioToken");
+        }
+    }
+}
---
+++ ControleContas/ControleContas.Infra.Data/Migrations/UsersDb/20210403134854_AlterarIdentity.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControleContas.Infra.Data.Migrations.UsersDb
+{
+    public partial class AlterarIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UsuarioToken",
+                table: "UsuarioToken");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UsuarioPapel",
+                table: "UsuarioPapel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PapelClaim",
+                table: "PapelClaim");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Papeis",
+                table: "Papeis");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Logins",
+                table: "Logins");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Claims",
+                table: "Claims");
+
+            migrationBuilder.RenameTable(
+                name: "UsuarioToken",
+                newName: "ASpNetIdUsuarioToken");
+
+            migrationBuilder.RenameTable(
+                name: "Usuarios",
+                newName: "ASpNetIdUsuarios");
+
+            migrationBuilder.RenameTable(
+                name: "UsuarioPapel",
+                newName: "ASpNetIdUsuarioPapel");
+
+            migrationBuilder.RenameTable(
+                name: "PapelClaim",
+                newName: "ASpNetIdPapelClaim");
+
+            migrationBuilder.RenameTable(
+                name: "Papeis",
+                newName: "ASpNetIdPapeis");
+
+            migrationBuilder.RenameTable(
+                name: "Logins",
+                newName: "ASpNetIdLogins");
+
+            migrationBuilder.RenameTable(
+                name: "Claims",
+                newName: "ASpNetIdClaims");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UsuarioPapel_RoleId",
+                table: "ASpNetIdUsuarioPapel",
+                newName: "IX_ASpNetIdUsuarioPapel_RoleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PapelClaim_RoleId",
+                table: "ASpNetIdPapelClaim",
+                newName: "IX_ASpNetIdPapelClaim_RoleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Logins_UserId",
+                table: "ASpNetIdLogins",
+                newName: "IX_ASpNetIdLogins_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Claims_UserId",
+                table: "ASpNetIdClaims",
+                newName: "IX_ASpNetIdClaims_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ASpNetIdUsuarioToken",
+                table: "ASpNetIdUsuarioToken",
+                columns: new[] { "UserId", "LoginProvider", "Name" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ASpNetIdUsuarios",
+                table: "ASpNetIdUsuarios",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ASpNetIdUsuarioPapel",
+                table: "ASpNetIdUsuarioPapel",
+                columns: new[] { "UserId", "RoleId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ASpNetIdPapelClaim",
+                table: "ASpNetIdPapelClaim",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ASpNetIdPapeis",
+                table: "ASpNetIdPapeis",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ASpNetIdLogins",
+                table: "ASpNetIdLogins",
+                columns: new[] { "LoginProvider", "ProviderKey" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ASpNetIdClaims",
+                table: "ASpNetIdClaims",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ASpNetIdUsuarioToken",
+                table: "ASpNetIdUsuarioToken");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ASpNetIdUsuarios",
+                table: "ASpNetIdUsuarios");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ASpNetIdUsuarioPapel",
+                table: "ASpNetIdUsuarioPapel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ASpNetIdPapelClaim",
+                table: "ASpNetIdPapelClaim");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ASpNetIdPapeis",
+                table: "ASpNetIdPapeis");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ASpNetIdLogins",
+                table: "ASpNetIdLogins");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ASpNetIdClaims",
+                table: "ASpNetIdClaims");
+
+            migrationBuilder.RenameTable(
+                name: "ASpNetIdUsuarioToken",
+                newName: "UsuarioToken");
+
+            migrationBuilder.RenameTable(
+                name: "ASpNetIdUsuarios",
+                newName: "Usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "ASpNetIdUsuarioPapel",
+                newName: "UsuarioPapel");
+
+            migrationBuilder.RenameTable(
+                name: "ASpNetIdPapelClaim",
+                newName: "PapelClaim");
+
+            migrationBuilder.RenameTable(
+                name: "ASpNetIdPapeis",
+                newName: "Papeis");
+
+            migrationBuilder.RenameTable(
+                name: "ASpNetIdLogins",
+                newName: "Logins");
+
+            migrationBuilder.RenameTable(
+                name: "ASpNetIdClaims",
+                newName: "Claims");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ASpNetIdUsuarioPapel_RoleId",
+                table: "UsuarioPapel",
+                newName: "IX_UsuarioPapel_RoleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ASpNetIdPapelClaim_RoleId",
+                table: "PapelClaim",
+                newName: "IX_PapelClaim_RoleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ASpNetIdLogins_UserId",
+                table: "Logins",
+                newName: "IX_Logins_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ASpNetIdClaims_UserId",
+                table: "Claims",
+                newName: "IX_Claims_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UsuarioToken",
+                table: "UsuarioToken",
+                columns: new[] { "UserId", "LoginProvider", "Name" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UsuarioPapel",
+                table: "UsuarioPapel",
+                columns: new[] { "UserId", "RoleId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PapelClaim",
+                table: "PapelClaim",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Papeis",
+                table: "Papeis",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Logins",
+                table: "Logins",
+                columns: new[] { "LoginProvider", "ProviderKey" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Claims",
+                table: "Claims",
+                column: "Id");
+        }
+    }
+}
